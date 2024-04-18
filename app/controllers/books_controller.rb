605f37160444389f0cca9ffca8fb8d2c2233e297
nyance-zyo <162596131+nyance-zyo@users.noreply.github.com>
2024-04-18 18:07:21 +0000

    3-12-1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,19 +4,40 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/top'
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:notice] = "投稿に失敗しました"
+      render :new
+    end
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
 
   private
   def book_params
